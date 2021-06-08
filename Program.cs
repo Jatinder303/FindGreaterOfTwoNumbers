fa35a7ca27b63df7d8b6d581ec0059b26d6e0f4c
jatinder <jatinder@NZSEHPE-29>
2021-06-08 19:49:21 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindGreaterOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Please enter first Number");
+            number1 = Convert.ToInt32(Console.ReadLine()); // This statement is converting the string into int
+
+            Console.WriteLine("Please enter second Number");
+            number2 = Convert.ToInt32(Console.ReadLine()); // This statement is converting the string into int
+
+            if(number1 > number2)
+            {
+                Console.WriteLine(number1.ToString() + " is greater than " + number2.ToString());
+            }
+
+            else
+            {
+                Console.WriteLine(number2.ToString() + " is greater than " + number1.ToString());
+            }
+        }
+    }
+}
